3ed182d362e0eac23daf27f2a690d500c9a216c3
Eugene Peelo <epeelo@users.noreply.github.com>
2011-07-31 19:56:04 +0000

    moved songbook-specific javascript to its own file.

---
+++ index.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+	jQuery('#playlist-holder ul ul').sortable({
+			connectWith: '.playlist ul'
+		});
+
+	jQuery('#playlist-holder>ul').sortable();
+
+	jQuery('#playlist-holder li li, #allsongs li').contextMenu('context-menu', {
+        'show lyrics': {
+            click: function(element){ location.href = '?action=displaySong&id=' + element.attr('id'); }
+        },
+        'edit song': {
+            click: function(element){ location.href = '?action=editSong&id=' + element.attr('id'); }
+        }
+      }
+	);
+
+	jQuery('#add-new-set').click(function () {
+		var newSet = jQuery('<li class=\"set playlist\"><textarea class=\"set-title\" type=\"text\">New Set</textarea></li>');
+		var newList = jQuery('<ul class=\"ui-sortable\"><li class=dummy>&nbsp;</li></ul>').sortable({
+			connectWith: '.playlist ul'
+		});
+		jQuery('#playlist-holder>ul').append(newSet);
+		newSet.append(newList);
+	});
+
+	jQuery('#savePlaylist').click(function() {
+		jQuery('#playlist-holder textarea').each(function(){jQuery(this).html(jQuery(this).val())});
+		jQuery('#playlist-holder li.song input.key').each(function(){jQuery(this).parent().attr('key', jQuery(this).val())}).remove();
+		jQuery('#playlist-holder li.song input.singer').each(function(){jQuery(this).parent().attr('singer', jQuery(this).val())}).remove();
+		jQuery('#playlist_input').val(jQuery('#playlist-holder').html());
+		jQuery('#playlistForm').submit();
+	});
+	function search_allsongs() {
+    	jQuery.get(
+		    '/songbook/allsongs_filterlist.php',
+		    {search_string: jQuery('#search_string').val()},
+    	    function (data) {
+    	    	jQuery('.all-song-list div#list').html(data);
+    	    	jQuery('.all-song-list #allsongsearch .number-of-records').html(jQuery('.all-song-list div#list .numberofrecords').html());
+    	    	if(jQuery('.displayPlaylist').length) {
+            		jQuery('#playlist-holder ul ul, #allsongs ul').sortable({
+            			connectWith: '.playlist ul'
+            		});
+        		}
+        		if(jQuery('.listAllSongs').length) {
+            		jQuery('#allsongs li').click(function () {
+            			location.href = '?action=displaySong&id=' + jQuery(this).attr('id');
+            		});
+        		}
+    	    }
+    	);
+    }
+    search_allsongs();
+	jQuery('form#allsongsearch').submit(function () {
+		search_allsongs();
+		return false;
+    });
+});
