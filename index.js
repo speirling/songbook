efebfcc83a734a0af6093bab5fbc2e1cc5ac482a
eugene <eugene@29973d71-0778-446b-8fbf-d5a3cbded35f>
2011-11-29 22:51:20 +0000

    chords can now include bass note modifiers

--- index.js
+++ index.js
@@ -242,7 +242,7 @@ function display_songpicker_from_playlist(parent_container, playlist) {
 		);
 }
 
-function sbk_charCode_to_chord_text(char_code) {
+function sbk_charCode_to_key_text(char_code) {
 	var codes = {
 		'65': 'A', //A
 		'66': 'B', //B
@@ -267,18 +267,43 @@ function sbk_charCode_to_chord_text(char_code) {
 	}
 }
 
+function sbk_charCode_to_bass_note(char_code) {
+	var codes = {
+		'65': 'a', //A
+		'66': 'b', //B
+		'67': 'c', //C
+		'68': 'd', //D
+		'69': 'e',
+		'70': 'f',
+		'71': 'g',
+		
+		'97': 'a', //a
+		'98': 'b', //b
+		'99': 'c', //c
+		'100': 'd', //d
+		'101': 'e',
+		'102': 'f',
+		'103': 'g'
+	};
+	if(typeof(codes[char_code]) === 'string') {
+		return codes[char_code];
+	} else {
+		return false;
+	}
+}
+
 function sbk_charCode_to_chord_modifier(char_code) {
 	//console.log(char_code);
 	var codes = {
 		'68': 'dim',
 		'100': 'dim',
+		'45': 'dim', //-
 		'109': 'm', 
 		'77': 'm',
 		'55': '7',
-		'43': 'aug',
-		'97': 'aug',
-		'65': 'aug',
-		'35': '#'
+		'43': 'aug', //+
+		'97': 'aug', //a
+		'65': 'aug', //A
 	};
 	if(typeof(codes[char_code]) === 'string') {
 		return codes[char_code];
@@ -288,32 +313,67 @@ function sbk_charCode_to_chord_modifier(char_code) {
 }
 
 function sbk_add_chord() {
-	var textarea, caret_position, chord_text = '', key = false;
+	var textarea, caret_position, chord_text = '', bass_note = false, bass_note_modifier = false;
 	
 	textarea = jQuery('textarea#content');
 
 	textarea.unbind('keypress').bind('keypress', function (event) {
-		if(chord_text.length === 0) {
-			key = sbk_charCode_to_chord_text(event.charCode);
+		var key = false;
+
+		//allow backspace to work
+		if(event.charCode === 0) { //backspace
+			chord_text = chord_text.slice(0, -1);
+			if (bass_note !== false) {
+				bass_note = bass_note - 1; 
+			}
+			if(bass_note === 0) {
+				bass_note = false;
+			}
+			return true;
 		} else {
-			//allow backspace to work
-			if(event.charCode === 0) {
-				chord_text = chord_text.slice(0, -1);
-				return true;
-			}
-			if(chord_text.length === 1) {
-				if(event.charCode == '66' | event.charCode == '98') {
-					chord_text = chord_text + 'b';
-					textarea.insertAtCaretPos('b');
+			if (bass_note === false) { //chord mode
+				if (chord_text.length === 0) { //the first character must be a key (capital)
+					key = sbk_charCode_to_key_text(event.charCode);
+				} else { //other notes
+					if(chord_text.length === 1) { // flat or sharp characters only allowed in second character
+						if(event.charCode == '66' | event.charCode == '98') { // B or b
+							chord_text = chord_text + 'b';
+							textarea.insertAtCaretPos('b');
+						}
+						if(event.charCode == '35') { // #
+							chord_text = chord_text + '#';
+							textarea.insertAtCaretPos('#');
+						}
+					}
+					if(event.charCode == '47') { // "/" - bass note coming - change to bass note mode
+						key = '/';
+						bass_note = 1;
+					} else {
+						key = sbk_charCode_to_chord_modifier(event.charCode);
+					}
 				}
+			} else { // bass note mode
+				if (bass_note === 1) { // first bass note character must be a key (lower case)
+					key = sbk_charCode_to_bass_note(event.charCode);
+					bass_note = bass_note + 1;
+				} else if (bass_note === 2) { //bass notes can only have two characters - a key and a flat or sharp
+					if(event.charCode == '66' | event.charCode == '98') { // B or b
+						chord_text = chord_text + 'b';
+						textarea.insertAtCaretPos('b');
+					}
+					if(event.charCode == '35') { // #
+						chord_text = chord_text + '#';
+						textarea.insertAtCaretPos('#');
+					}
+					bass_note = bass_note + 1;
+				} 
 			}
-			key = sbk_charCode_to_chord_modifier(event.charCode);
+			if (key) {
+				chord_text = chord_text + key;
+				textarea.insertAtCaretPos(key);
+			}
+			return false;
 		}
-		if(key) {
-			chord_text = chord_text + key;
-			textarea.insertAtCaretPos(key);
-		}
-		return false;
 	});
 	textarea.insertAtCaretPos('[');
 	textarea.insertAtCaretPos(']');
