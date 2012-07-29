5dad2127b8b5bb71cd6e953435330c8cf3430bd5
Eugene Peelo <epeelo@users.noreply.github.com>
2012-07-29 19:15:45 +0000

    playlist picker grouped by act

--- js_classes/SBK.Class.AllPlaylists.js
+++ js_classes/SBK.Class.AllPlaylists.js
@@ -19,12 +19,31 @@ SBK.AllPlaylists = SBK.Class.extend({
 		self.http_request.api_call(
 		    {action: 'get_available_playlists'},
 		    function (response) {
-		    	all_playlists = jQuery(self.template.render(response.data)).appendTo(self.container);
+		    	playlist_grouped_by_act = {acts: self.group_by_act(response.data)};
+		    	all_playlists = jQuery(self.template.render(playlist_grouped_by_act)).appendTo(self.container);
 		    	if(typeof(callback) === 'function') {
 		    		callback(all_playlists);
 		    	}
 		    	self.pleasewait.hide();
     		}
 		);
+	},
+
+	group_by_act: function (data) {
+		var self = this, index, playlist_grouped_by_act = [], act, act_index = {};
+
+		for (index = 0; index < data.playlists.length; index = index + 1) {
+			act = data.playlists[index].act;
+			if(act === '') {
+				act = 'unknown';
+			}
+			if(typeof(act_index[act]) === 'undefined') {
+				act_index[act] = playlist_grouped_by_act.length;
+				playlist_grouped_by_act[act_index[act]] = {act: act, playlists: [data.playlists[index]]};
+			} else {
+				playlist_grouped_by_act[act_index[act]].playlists.push(data.playlists[index]);
+			}
+		}
+		return  playlist_grouped_by_act;
 	}
 });
