664c2208935c8d661df66eb6d985f9ac8bde3bad
Eugene Peelo <eugene@fps.defaultdomain>
2015-10-18 17:56:43 +0100

    [33] Initalise js app.

--- src/Template/Layout/default.ctp
+++ src/Template/Layout/default.ctp
@@ -21,37 +21,42 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
     <?= $this->Html->charset() ?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>
-        <?= $cakeDescription ?>:
+        EP Songbook:
         <?= $this->fetch('title') ?>
     </title>
     <?= $this->Html->meta('icon') ?>
 
     <?= $this->Html->css('base.css') ?>
     <?= $this->Html->css('cake.css') ?>
+    <?= $this->Html->css(Cake\Core\Configure::read('Songbook.css_library')); ?>
+	<?= $this->Html->script(Cake\Core\Configure::read('Songbook.js_library')); ?>
+    
 
     <?= $this->fetch('meta') ?>
     <?= $this->fetch('css') ?>
     <?= $this->fetch('script') ?>
 </head>
 <body>
-    <nav class="top-bar expanded" data-topbar role="navigation">
-        <ul class="title-area large-3 medium-4 columns">
-            <li class="name">
-                <h1><a href=""><?= $this->fetch('title') ?></a></h1>
-            </li>
-        </ul>
-        <section class="top-bar-section">
-            <ul class="right">
-                <li><a target="_blank" href="http://book.cakephp.org/3.0/">Documentation</a></li>
-                <li><a target="_blank" href="http://api.cakephp.org/3.0/">API</a></li>
+    <div id="container">
+        <nav class="top-bar expanded" data-topbar role="navigation">
+            <ul class="title-area large-3 medium-4 columns">
+                <li class="name">
+                    <h1><a href=""><?= $this->fetch('title') ?></a></h1>
+                </li>
             </ul>
+            <section class="top-bar-section">
+                <ul class="right">
+                    <li><a target="_blank" href="http://book.cakephp.org/3.0/">Documentation</a></li>
+                    <li><a target="_blank" href="http://api.cakephp.org/3.0/">API</a></li>
+                </ul>
+            </section>
+        </nav>
+        <?= $this->Flash->render() ?>
+        <section class="container clearfix">
+            <?= $this->fetch('content') ?>
         </section>
-    </nav>
-    <?= $this->Flash->render() ?>
-    <section class="container clearfix">
-        <?= $this->fetch('content') ?>
-    </section>
-    <footer>
-    </footer>
+        <footer>
+        </footer>
+    </div>
 </body>
 </html>
--- webroot/index.php
+++ webroot/index.php
@@ -30,6 +30,70 @@ use Cake\Network\Request;
 use Cake\Network\Response;
 use Cake\Routing\DispatcherFactory;
 
+
+/* Songbook configuration */
+Cake\Core\Configure::write(
+    'Songbook', array(
+        'playlist_directory' => '/fileserver/data/playlists',
+        'js_library' => array(
+            'js_dependencies/jquery-1.10.2.js',
+            'js_dependencies/jquery-ui-1.10.4.custom.min.js',
+            'js_dependencies/jsrender.min.js',
+            'js_classes/SBK.Namespace.js',
+            'js_classes/SBK.Constants.js',
+            'js_classes/SBK.StaticFunctions.js',
+            'js_classes/SBK.StaticFunctions.LyricChordHTML.js',
+            'js_classes/SBK.Class.js',
+            'js_classes/SBK.Class.CallbackList.js',
+            'js_classes/SBK.Class.ApplicationState.js',
+            'js_classes/SBK.Class.PleaseWait.js',
+            'js_classes/SBK.Class.Button.js',
+            'js_classes/SBK.Class.ChordEditor.js',
+            'js_classes/SBK.Class.HTTPRequest.js',
+            'js_classes/SBK.Class.Api.js',
+            'js_classes/SBK.Class.SongList.js',
+            'js_classes/SBK.Class.SongList.PlayList.js',
+            'js_classes/SBK.Class.SongList.PlayList.Alphabetical.js',
+            'js_classes/SBK.Class.SongList.PlayList.Book.js',
+            'js_classes/SBK.Class.SongList.PlayList.Selector.js',
+            'js_classes/SBK.Class.SongList.PlayList.Table.js',
+            'js_classes/SBK.Class.SongList.PlayList.Text.js',
+            'js_classes/SBK.Class.SongList.SongFilterList.js',
+            'js_classes/SBK.Class.SongList.SongFilterList.Lyrics.js',
+            'js_classes/SBK.Class.SongPicker.js',
+            'js_classes/SBK.Class.AllPlaylists.js',
+            'js_classes/SBK.Class.AllPlaylists.PlaylistPicker.js',
+            'js_classes/SBK.Class.PaginatedHTML.js',
+            'js_classes/SBK.Class.SongbookApplication.js',
+            'js_classes/SBK.Class.SongListItemSet.js',
+            'js_classes/SBK.Class.SongListItemSet.Selector.js',
+            //'js_classes/SBK.Class.SongListItemSet.Print.js',
+            'js_classes/SBK.Class.SongListItemSong.js',
+            'js_classes/SBK.Class.SongListItemSong.Selector.js',
+            'js_classes/SBK.Class.SongListItemSong.Lyrics.js',
+            //'js_classes/SBK.Class.SongListItemSong.Print.js',
+            'js_classes/SBK.Class.SongLyricsDisplay.js',
+            'js_classes/SBK.Class.SongLyricsEdit.js'
+        ),
+        'css_library' => array(
+            'common.css',
+            'playlists-list.css',
+            'songs-list.css',
+            'playlist-book.css',
+            'playlist-edit.css',
+            'playlist-print.css',
+            'song-edit.css',
+            'song-lyrics.css',
+            'chord_editor.css',
+            'baked_ui.css'
+        )
+    )
+);
+/* end Songbook configuration */
+
+
+
+
 $dispatcher = DispatcherFactory::create();
 $dispatcher->dispatch(
     Request::createFromGlobals(),
