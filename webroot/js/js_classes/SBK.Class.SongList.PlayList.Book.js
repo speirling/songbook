f7edfd40b1594b0cbd4040901f3fb6d6caac82a2
Eugene Peelo <eugene@fps.defaultdomain>
2016-02-28 16:54:10 +0000

    [34] Because the pdf printers that are add-ons to web browsers don't properly support css columns, and the microsoft save as pdf driver doesn't put in hyperlinks, I've refactored the playlist-book to use manual columns - splitting contetn across table cells in javascript. These give adequate pdf outputs from the database, so that the database can be accessible offline. It's not perfect but it's useable. if doesn't use the paginated html class either though I thought this less flexible approach is simpler, just till pdf printers catch up.

--- webroot/js/js_classes/SBK.Class.SongList.PlayList.Book.js
+++ webroot/js/js_classes/SBK.Class.SongList.PlayList.Book.js
@@ -39,18 +39,27 @@ SBK.PlayList.Book = SBK.PlayList.extend({
     },
 
     to_html: function (data_json) {
-        var self = this, playlist_HTML, set_index, song_index, set_ol, ul, input_container_title, input_container_act, internal_navigation_bar, filter_to;
-
-        playlist_HTML = '<div class="playlist list print-page">';
-        playlist_HTML = playlist_HTML + '<span class="playlist-title"><label>Playlist: </label>' + self.value_or_blank(data_json.title) + '</span>';
-        playlist_HTML = playlist_HTML + '<span class="playlist-act"><label>Act: </label>' + self.value_or_blank(data_json.act) + '</span>';
-        playlist_HTML = playlist_HTML + '<ul>';
+        var self = this, page_html = '', set_index, song_index, set_ol, ul, input_container_title, input_container_act, internal_navigation_bar, filter_to;
 
         lines_per_set_title = 2;
         lines_per_song = 1;
         lines_per_page = 88;
         line_count = 0;
         page_counter = 1;
+
+        page_html = page_html + '<div class="playlist list print-page">';
+        // ---start of delete when pdf printer is available ----------------------------
+        // until I can get a pdf printer that both acknowledges columns (like microsoft pdf printer) _and_ hyperlinks (like chrome save as pdf) I'd better manually create columns
+        column_count = 1;
+        page_html = page_html + '<table class="manual-column"><tbody><tr>';
+        page_html = page_html + '<td class="column_' + column_count + '">';
+        // ---end of delete when pdf printer is available ----------------------------
+        
+        page_html = page_html + '<span class="playlist-title"><label>Playlist: </label>' + self.value_or_blank(data_json.title) + '</span>';
+        page_html = page_html + '<span class="playlist-act">(<label>Act: </label>' + self.value_or_blank(data_json.act) + ')</span>';
+        line_count = line_count + 2; // playlist title and act name
+
+        page_html = page_html + '<ul>';
         
         if (typeof(data_json.sets) !== 'undefined') { //could happen when a playlist is first defined
             data_json.sets = [].concat(data_json.sets); //same issue as WORKAROUND below
@@ -63,32 +72,51 @@ SBK.PlayList.Book = SBK.PlayList.extend({
                     data_json.sets[set_index].songs = [].concat(data_json.sets[set_index].songs);
                 }
 
-                playlist_HTML = playlist_HTML + '<li class="set" id="set_' + set_index + '">' + 
+                page_html = page_html + '<li class="set" id="set_' + set_index + '">' + 
                 		'<span class="set-title"><label><span>Set: </span></label><span type="text" class="set-title">' + SBK.StaticFunctions.value_or_blank(set_data.label) + '</span></span>';
 
-                playlist_HTML = playlist_HTML + '<ol class="songlist">';
+                page_html = page_html + '<ol class="songlist">';
 
                 if (typeof(set_data.songs) !== 'undefined') {
                     for (song_index = 0; song_index < set_data.songs.length; song_index = song_index + 1) {
                     	line_count = line_count + lines_per_song;
+
                     	if ((line_count / lines_per_page) > page_counter) {
                     		page_counter = page_counter + 1;
-                    		playlist_HTML = playlist_HTML + '</ul></div><div class="playlist list print-page"><ul>';
+                    		// ------------------------------
+                    		page_html = page_html + '</ol></td></tr></tbody></table>'; //end column
+                    		// ------------------------------
+                    		page_html = page_html + '</ol></div>'; //end page
+                    		
+                    		column_count = 1;
+                            line_count = 0;
+                    		page_html = page_html + '<div class="playlist list print-page"><ol class="songlist">'; //start new page
+                    		// ------------------------------
+                            page_html = page_html + '<table class="manual-column"><tbody><tr>';
+                            page_html = page_html + '<td class="column_' + column_count + '"><ol class="songlist">'; //start new column
+                    		// ------------------------------
+                    	} else if ((column_count === 1) && (line_count > (lines_per_page / 2))) {
+                    		column_count = column_count + 1;
+                    		page_html = page_html + '</td>'; //end column
+                            page_html = page_html + '<td class="column_' + column_count + '"><ol class="songlist">'; //start new column
                     	}
                     	song_data = set_data.songs[song_index];
-                    	playlist_HTML = playlist_HTML + '<li class="song"><span class="title-bar"><span class="key">' + SBK.StaticFunctions.value_or_blank(song_data.key) + '</span>' + 
+                    	page_html = page_html + '<li class="song"><span class="title-bar"><span class="key">' + SBK.StaticFunctions.value_or_blank(song_data.key) + '</span>' + 
                         		'<a href="#song_' + SBK.StaticFunctions.value_or_blank(song_data.id) + '" class="title">' + SBK.StaticFunctions.value_or_blank(song_data.title) + '</a>' + 
                         		'</span></li>';                       
                     }
                 }
-                playlist_HTML = playlist_HTML + '</ol>';
-                playlist_HTML = playlist_HTML + '</li>';
+                page_html = page_html + '</ol>';
+                page_html = page_html + '</li>';
             }
         } else {
             console.log('no sets');
         }
-        playlist_HTML = playlist_HTML + '</ul></div>';
+        // ----------------------------------------------
+        page_html = page_html + '</td></tr></tbody></table>'; //end column
+        // ----------------------------------------------
+        page_html = page_html + '</ul></div>';
 
-        return playlist_HTML;
+        return page_html;
     }
 });
--- webroot/js/js_classes/SBK.Class.SongLyricsDisplay.Book.js
+++ webroot/js/js_classes/SBK.Class.SongLyricsDisplay.Book.js
@@ -3,7 +3,7 @@
 SBK.SongLyricsDisplay.Book = SBK.SongLyricsDisplay.extend({
 
     render_response: function (response, buttons_displayed, paginated) {
-        var self = this, target_key_container, song_data, key_container, capo_container, lines, page;
+        var self = this, target_key_container, song_data, key_container, capo_container, lines, page_html = '';
 
         song_data = response.data;
         
@@ -32,20 +32,27 @@ SBK.SongLyricsDisplay.Book = SBK.SongLyricsDisplay.extend({
 
         //new SBK.PaginatedHTML(self.container, '.page-header', 'song-page', '.content');
         //instead of using the tabular layout of SBK.PaginatedHTML, maybe just empirically determine the number of <line>s that can fit on a page columned by CSS3
-        lines_per_page = 48;
+        lines_per_page = 58;
         characters_per_line = 100;
         height_of_line_with_chords = 1;
+        columns_class = '';
         //and after that number, insert a new page
         text_to_nodes_conversion_container = jQuery('<div></div>');
         text_to_nodes_conversion_container.html(self.song_content_to_html(song_data.content));
         lines = jQuery('div.line', text_to_nodes_conversion_container);
-
-        page = jQuery('<div class="lyrics-page print-page">' + header_HTML + '</div>').appendTo(self.container);
+        
         if(lines.length > (lines_per_page / 2)) {
         	columns_class = 'columns-2'
             characters_per_line = characters_per_line / 2;
         }
-        lyrics_panel = jQuery('<div class="lyrics-panel ' + columns_class + '"></div>').appendTo(page);
+        page_html = page_html + '<div class="lyrics-page print-page lyrics-content ">' + header_HTML; //start new page
+        page_html = page_html + '<div class="lyrics-panel ' + columns_class + '">'; //start new lyrics panel
+        // ---start of delete when pdf printer is available ----------------------------
+        // until I can get a pdf printer that both acknowledges columns (like microsoft pdf printer) _and_ hyperlinks (like chrome save as pdf) I'd better manually create columns
+        column_count = 1;
+        page_html = page_html + '<table class="manual-column"><tbody><tr>';
+        page_html = page_html + '<td class="column_' + column_count + '">';
+        // ---end of delete when pdf printer is available ----------------------------
         page_counter = 1;
         line_count = 0;
         for (index = 0; index < lines.length; index = index + 1) {
@@ -56,15 +63,38 @@ SBK.SongLyricsDisplay.Book = SBK.SongLyricsDisplay.extend({
         	}
         	if ((line_count / lines_per_page) > page_counter) {
         		page_counter = page_counter + 1;
-        		page = jQuery('<div class="lyrics-page print-page lyrics-content following-page">' + header_HTML + '</div>').appendTo(self.container);
         		if ((lines.length - (page_counter * lines_per_page)) > (lines_per_page / 2)) {
                 	columns_class = 'columns-2'
                     characters_per_line = characters_per_line / 2;
                 }
-                lyrics_panel = jQuery('<div class="lyrics-panel ' + columns_class + '"></div>').appendTo(page);
+        		// ------------------------------
+        		page_html = page_html + '</td></tr></tbody></table>'; //end column
+        		// ------------------------------
+        		page_html = page_html + '</div>'; //end lyrics panel
+        		page_html = page_html + '</div>'; //end page
+
+                column_count = 1;
+                line_count = 0;
+        		page_html = page_html + '<div class="lyrics-page print-page lyrics-content following-page">' + header_HTML; //start new page
+        		page_html = page_html + '<div class="lyrics-panel ' + columns_class + '">'; //start new lyrics panel
+        		// ------------------------------
+                page_html = page_html + '<table class="manual-column"><tbody><tr>';
+                page_html = page_html + '<td class="column_' + column_count + '">'; //start new column
+        		// ------------------------------                
+        	} else if ((column_count === 1) && (line_count > (lines_per_page / 2))) {
+        		column_count = column_count + 1;
+        		page_html = page_html + '</td>'; //end column
+                page_html = page_html + '<td class="column_' + column_count + '">'; //start new column
         	}
-        	lyrics_panel.append(lines[index]);
+        	page_html = page_html + '<div class="line">' + jQuery(lines[index]).html() + '</div>';
         }
-        
+
+        // ----------------------------------------------
+        page_html = page_html + '</td></tr></tbody></table>'; //end column
+        // ----------------------------------------------
+		page_html = page_html + '</div>'; //end lyrics panel
+        page_html = page_html + '</div>'; //end page
+console.log(page_html);
+        jQuery(page_html).appendTo(self.container);
     }
 });
