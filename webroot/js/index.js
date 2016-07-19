e6be0bc8899355e43b5d7bbe34f86c8d0895e4ca
Eugene Peelo <eugene@fps.defaultdomain>
2016-07-19 17:21:18 +0100

    [38] Fixed an incompatibility between the Javascript app and the CakePHP app - a style that affected the Cake version adverseley, and error trapping for the Document Ready function because it runs with the JS app also.

--- webroot/js/index.js
+++ webroot/js/index.js
@@ -1,5 +1,8 @@
 /*global jQuery document */
 
 jQuery(document).ready(function() {
-     new SBK.ChordEditor(jQuery('.sbk-lyrics-panel')).render();
+	var lyrics_panels = jQuery('.sbk-lyrics-panel');
+	if(lyrics_panels.length > 0) {
+	    new SBK.ChordEditor(jQuery('.sbk-lyrics-panel')).render();
+	}
 });
