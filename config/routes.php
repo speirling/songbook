9fe9e9db701608938ee4e1eaec631c8463b9e9ed
Eugene Peelo <eugene@fps.defaultdomain>
2017-02-28 23:58:24 +0000

    [#57] Resetting the home page - when it was set to dashboard the search form became /songbook ... and wouldn't work.

--- config/routes.php
+++ config/routes.php
@@ -48,7 +48,7 @@ Router::scope('/', function ($routes) {
      * to use (in this case, src/Template/Pages/home.ctp)...
      */
     //$routes->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
-    $routes->connect('/', ['controller' => 'Dashboard', 'action' => 'index']);
+    $routes->connect('/', ['controller' => 'Songs', 'action' => 'index']);
 
     /**
      * ...and connect the rest of 'Pages' controller's URLs.
